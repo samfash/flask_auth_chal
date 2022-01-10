306cf8ebda3d77e3808bb5dcc2d726fea40b4131
samuel <samuelfasanya35@yahoo.com>
2022-01-10 15:14:57 -0800

    complete blog

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, EmailField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
 
